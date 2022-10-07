e70ffc4149f35bd42dbeed4fa4de183ee47b63a7
Wysogota <kmmaxim69@gmail.com>
2022-10-07 17:17:16 +0300

    Catch errors

--- server/controllers/conference.controller.php
+++ server/controllers/conference.controller.php
@@ -3,40 +3,60 @@ require_once APP_DIR . '/models/Conference.php';
 
 function getConferenceById($request)
 {
-  $conferenceId = $request->params['conferenceId'];
-  $conference = Conference::findOneById($conferenceId);
-  http_response_code(200);
-  return json_encode($conference);
+  try {
+    $conferenceId = $request->params['conferenceId'];
+    $conference = Conference::findOneById($conferenceId);
+    http_response_code(200);
+    return json_encode($conference);
+  } catch (PDOException $e) {
+    return json_encode(['error' => $e->getMessage()]);
+  }
 }
 
 function getConferences($request)
 {
-  $conferences = Conference::findAll();
-  http_response_code(200);
-  return json_encode($conferences);
+  try {
+    $conferences = Conference::findAll();
+    http_response_code(200);
+    return json_encode($conferences);
+  } catch (PDOException $e) {
+    return json_encode(['error' => $e->getMessage()]);
+  }
 }
 
 function createConference($request)
 {
-  $body = $request->getBody();
-  $conference = Conference::create($body, true);
-  http_response_code(201);
-  return json_encode($conference);
+  try {
+    $body = $request->getBody();
+    $conference = Conference::create($body, true);
+    http_response_code(201);
+    return json_encode($conference);
+  } catch (PDOException $e) {
+    return json_encode(['error' => $e->getMessage()]);
+  }
 }
 
 function updateConference($request)
 {
-  $conferenceId = $request->params['conferenceId'];
-  $body = $request->getBody();
-  $conference = Conference::updateById($conferenceId, $body, true);
-  http_response_code(200);
-  return json_encode($conference);
+  try {
+    $conferenceId = $request->params['conferenceId'];
+    $body = $request->getBody();
+    $conference = Conference::updateById($conferenceId, $body, true);
+    http_response_code(200);
+    return json_encode($conference);
+  } catch (PDOException $e) {
+    return json_encode(['error' => $e->getMessage()]);
+  }
 }
 
 function deleteConference($request)
 {
-  $conferenceId = $request->params['conferenceId'];
-  $conference = Conference::deleteById($conferenceId, true);
-  http_response_code(200);
-  return json_encode($conference);
+  try {
+    $conferenceId = $request->params['conferenceId'];
+    $conference = Conference::deleteById($conferenceId, true);
+    http_response_code(200);
+    return json_encode($conference);
+  } catch (PDOException $e) {
+    return json_encode(['error' => $e->getMessage()]);
+  }
 }
