845acdef066733c1cd42a9691f935ef30a87e39b
Wysogota <kmmaxim69@gmail.com>
2022-10-08 18:56:18 +0300

    Add cors func

--- server/index.php
+++ server/index.php
@@ -4,6 +4,9 @@ define('APP_DIR', $_SERVER['DOCUMENT_ROOT']);
 require_once './Router/Request.php';
 require_once './Router/Router.php';
 require_once './routes/routes.php';
+require_once './functions/cors.php';
+
+cors();
 
 $router = new Router(new Request());
 
