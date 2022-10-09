9ee854a6073a2f21ad4e7b237c7c98d28e29e972
Wysogota <kmmaxim69@gmail.com>
2022-10-09 17:05:18 +0300

    Update Header style

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,11 +1,11 @@
-import { Container, Row, Col, } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Logo from '../Logo';
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <Container as='header' className={styles.header} fluid>
-      <Row>
+      <Row className='h-100 align-content-center'>
         <Col xs='4' md='2'>
           <Logo />
         </Col>
