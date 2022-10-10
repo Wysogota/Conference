b11c9f1b6ecff98ccc28c50f8aee367ec7bc471b
Wysogota <kmmaxim69@gmail.com>
2022-10-10 11:12:05 +0300

    Update formatting config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,14 +12,26 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: [
     'react',
   ],
   rules: {
-    quotes: [1, 'single',],
-    semi: [1, 'always',],
-    'comma-dangle': [1, 'always',],
+    quotes: [1, 'single'],
+    semi: [1, 'always'],
+    'comma-dangle': [1, {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'always-multiline',
+      imports: 'always-multiline',
+    }],
     'react/react-in-jsx-scope': 'off',
+    'no-trailing-spaces': [1, { skipBlankLines: true }],
+    'no-unused-vars': 1,
+    camelcase: 0,
+    'react/jsx-indent': [1, 2],
   },
 };
