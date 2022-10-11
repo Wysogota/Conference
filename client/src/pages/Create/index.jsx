814376db2a75e71bbe5fc367735ed66371b980a4
Wysogota <kmmaxim69@gmail.com>
2022-10-11 13:55:28 +0300

    Redirect to created conference

--- client/src/pages/Create/index.jsx
+++ client/src/pages/Create/index.jsx
@@ -6,9 +6,22 @@ import { capitalize } from 'lodash';
 import CreationForm from '../../components/Conference/CreationForm';
 import { observer } from 'mobx-react-lite';
 import { conferenceStore } from '../../store';
+import { useNavigate } from 'react-router-dom';
+import useAfterRequestAction from '../../hooks/useAfterRequestAction';
+import CONSTANTS from '../../constants';
+const { PAGES: { HOME } } = CONSTANTS;
 
 const Create = observer(() => {
-  const { create } = conferenceStore;
+  const { create, conference, isFetching, error } = conferenceStore;
+
+  const navigate = useNavigate();
+  const action = () => navigate(HOME + conference.id);
+  const { setIsRequested } = useAfterRequestAction(action, isFetching, error);
+
+  const submitAction = (conference) => {
+    setIsRequested(true);
+    create(conference);
+  };
 
   const headerClasses = cx(theme, 'pt-4 pb-3 mb-5 rounded-bottom');
   const bodyClasses = cx(theme, 'p-3 rounded m-auto');
@@ -23,7 +36,7 @@ const Create = observer(() => {
       <Col md='10' lg='8' className={bodyClasses}>
         <Row>
           <Col>
-            <CreationForm submitAction={create} />
+            <CreationForm submitAction={submitAction} />
           </Col>
         </Row>
         <Row>
